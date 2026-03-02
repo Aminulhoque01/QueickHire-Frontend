ef834ac1ec3d3467129c656080a1c87c2c18ccfb
Aminulhoque01 <aminuhoque53@gmail.com>
2026-03-02 23:52:41 +0600

    build done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,11 +4,10 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
- 
-
 import { useLoginMutation } from "@/redux/features/services/authApi";
 import { setCredentials } from "@/redux/features/authSlice";
 import { jwtDecode } from "jwt-decode";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 interface JwtPayload {
   id: string;
@@ -20,6 +19,7 @@ interface JwtPayload {
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const [login, { isLoading }] = useLoginMutation();
   const dispatch = useDispatch();
@@ -30,11 +30,10 @@ export default function LoginPage() {
 
     try {
       const res = await login({ email, password }).unwrap();
-
       const token = res.data.token;
+
       localStorage.setItem("token", token);
 
-      // Decode token to get user info
       const decoded: JwtPayload = jwtDecode(token);
 
       dispatch(
@@ -58,19 +57,39 @@ export default function LoginPage() {
   return (
     <div className="mt-10 py-20">
       <form onSubmit={handleSubmit} className="max-w-md mx-auto space-y-4">
+        
         <input
           type="email"
           placeholder="Email"
-          className="w-full border p-2"
+          className="w-full border p-2 rounded"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border p-2"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-[#4640DE] text-white px-4 py-2 w-full">
+
+        {/* Password Input With Toggle */}
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            className="w-full border p-2 rounded pr-10"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <span
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-2.5 cursor-pointer text-gray-500"
+          >
+            {showPassword ? (
+              <AiOutlineEyeInvisible size={20} />
+            ) : (
+              <AiOutlineEye size={20} />
+            )}
+          </span>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-[#4640DE] text-white px-4 py-2 w-full rounded"
+        >
           {isLoading ? "Loading..." : "Login"}
         </button>
       </form>
